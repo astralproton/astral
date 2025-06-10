18ced89679a9ffbcec9498494c3928e80ea87871
astral <yisusunai@gmail.com>
2025-06-09 21:55:27 -0700

    Act6935

---
+++ Juegos/AstralMusic/respaldo.js
@@ -0,0 +1,9 @@
+        server1Btn.addEventListener('click', function() {
+            window.location.href = "server1premium.html";
+        });
+        server2Btn.addEventListener('click', function() {
+            window.location.href = "server2premium.html";
+        });
+        publicoBtn.addEventListener('click', function() {
+            window.location.href = "public.html";
+        });
